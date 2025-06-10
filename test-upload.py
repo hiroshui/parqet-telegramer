a0425298393ef8df5f553c4ed2b4dca36fe68984
Maximilian Krone <m.krone@fme.de>
2025-06-10 23:24:09 +0200

    feat: add upload-testfile

---
+++ test-upload.py
@@ -0,0 +1,20 @@
+import os
+import asyncio
+from playwright.sync_api import sync_playwright
+from upload import upload_pdf
+from dotenv import load_dotenv
+
+load_dotenv()
+PARQET_ID = os.getenv("PARQET_ID")
+FILE_PATH=os.getenv("FILE_PATH")
+
+if FILE_PATH == None:
+    raise IOError("FILE_PATH was not set. Please check your env-file or set it via export.")
+
+async def upload():
+    print("start upload")
+    await upload_pdf(pdf_path=FILE_PATH, parqet_id=PARQET_ID)
+    
+# Start the upload process as asny using asnycio.
+if __name__ == "__main__":
+    asyncio.run(upload())
