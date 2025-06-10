0d455d68814c966f944b5d8347c5719837848a10
Maximilian Krone <m.krone@fme.de>
2025-06-10 23:43:42 +0200

    refact: add a check for auth.json

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from playwright.sync_api import sync_playwright
 
 import os
@@ -6,8 +7,12 @@ with sync_playwright() as p:
     website="https://example.com"
     if os.getenv("WEBSITE") != None:
         website=os.getenv("WEBSITE")
-    
+
     browser = p.chromium.launch(headless=True)
+    auth_file = Path("auth.json")
+    if auth_file.exists("auth.json"):
+        context = browser.new_context(storage_state="auth.json")
+
     page = browser.new_page()
     page.goto(website)
     print(page.content())
