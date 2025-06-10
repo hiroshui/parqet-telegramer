a4a19b05d2d2b3ed0409c4063be9af177b9a493c
Maximilian Krone <m.krone@fme.de>
2025-06-10 23:35:24 +0200

    refact: make website changable via env

--- test.py
+++ test.py
@@ -1,8 +1,14 @@
 from playwright.sync_api import sync_playwright
 
+import os
+
 with sync_playwright() as p:
+    website="https://example.com"
+    if os.getenv("WEBSITE") != None:
+        website=os.getenv("WEBSITE")
+    
     browser = p.chromium.launch(headless=True)
-    page = browser.new_page()
-    page.goto("https://example.com")
+    page = browser.new_page(website)
+    page.goto(website)
     print(page.content())
     browser.close()
